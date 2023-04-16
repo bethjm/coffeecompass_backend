4caba167c9392683620badc2f03db12eeddb58e9
BethCries <b.jeanne.mills@gmail.com>
2023-04-16 16:37:00 -0400

    removed comments info

--- coffee_compass_api/cafes_api/serializers.py
+++ coffee_compass_api/cafes_api/serializers.py
@@ -5,10 +5,7 @@ from .models import Shops
 class ShopsSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Shops 
-        fields = ('id', 'photo', 'name', 'phone','online_order', 'address', 'description','flavor_notes')
+        fields = ('id', 'photo', 'name', 'phone','online_order', 'address', 'description','sweet', 'acidic','floral', 'citrus', 'berry', 'chocolate', 'caramel', 'smoky', 'bitter', 'best_type', 'price')
 
 
-class CommentsSerializer(serializers.ModelSerializer): 
-    class Meta:
-        model = Comments 
-        fields = ('id', 'person_name', 'coffee_name', 'origin','beverage_type', 'roaster', 'producer','price', 'comment', 'sweet','acidic', 'floral', 'citrus','berry', 'chocolate', 'caramel','smoky', 'bitter', 'clean','shops')
+
--- coffee_compass_api/cafes_api/urls.py
+++ coffee_compass_api/cafes_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/shops', views.ShopsList.as_view(), name='shops_list'), # api/contacts will be routed to the ContactList view for handling
+
     path('api/shops/<int:pk>', views.ShopsDetail.as_view(), name='shops_detail'), # api/contacts will be routed to the ContactDetail view for handling
 ]
